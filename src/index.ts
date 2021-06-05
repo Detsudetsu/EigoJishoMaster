0b591f46dd3757ecf30699826b6caad2a7715c0a
detsu <tubopi311@gmail.com>
2021-06-05 19:46:12 +0900

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const search_box = document.getElementById("search_box")
+const search_text = document.getElementById("search_text")
+if (!(search_text instanceof HTMLInputElement)) throw new TypeError();
+
+search_box.addEventListener("submit",e=>
+{
+    const query = search_text.value
+    window.open("https://www.google.com/search?q=meaning+" + query)
+    window.open("https://eow.alc.co.jp/search?q=" + query)
+    window.open("https://www.etymonline.com/search?q=" + query)
+    e.preventDefault()
+})
