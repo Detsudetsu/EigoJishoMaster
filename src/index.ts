ccb52b126566b5b27dd00d26daa976cc87cfe20a
detsu <tubopi311@gmail.com>
2021-06-07 11:56:51 +0900

    minor formatting

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 const search_box = document.getElementById("search_box")
 const search_text = document.getElementById("search_text")
-if (!(search_text instanceof HTMLInputElement)) throw new TypeError();
+if (!(search_text instanceof HTMLInputElement)) throw new TypeError()
 
-search_box.addEventListener("submit",e=>
-{
+search_box.addEventListener("submit", e => {
     const query = search_text.value
     window.open("https://www.google.com/search?q=meaning+" + query)
     window.open("https://eow.alc.co.jp/search?q=" + query)
