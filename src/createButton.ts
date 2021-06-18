29b86b3de13d2ae8a00a3144d9f3699211d4ffbb
detsu <tubopi311@gmail.com>
2021-06-19 05:49:12 +0900

    Visual fix

--- src/createButton.ts
+++ src/createButton.ts
@@ -2,6 +2,6 @@ export function createButton(item: string) {
     const button = document.createElement("button")
     button.value = item
     button.textContent = item
-    button.setAttribute("class", "btn btn-outline-primary btn-light")
+    button.setAttribute("class", "btn btn-outline-light text-start text-secondary border-start-0 border-end-0")
     return button
 }
