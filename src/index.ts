8192f3d8cf8119e3574b4b7016e63622cec90673
detsu <tubopi311@gmail.com>
2021-06-19 18:01:40 +0900

    Shift focus with arrow keys

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,34 @@ const search_box = document.getElementById("search_box")
 const search_text = document.getElementById("search_text")
 if (!(search_text instanceof HTMLInputElement)) throw new TypeError()
 const suggestionGroup = document.getElementById("suggestions")
+const suggestionButtons = suggestionGroup.getElementsByTagName("button")
+
+search_box.addEventListener("keydown", e => {
+    switch (e.code) {
+        case "ArrowDown":
+            if (suggestionButtons.length === 0) break
+            const isFocusOnSearchBar = document.activeElement === search_text
+            const next = isFocusOnSearchBar ? suggestionButtons[0] : document.activeElement.nextElementSibling
+            tryFocus(next)
+            break
+        case "ArrowUp":
+            const isFocusOnTopButton = document.activeElement === suggestionButtons[0]
+            const prev = isFocusOnTopButton ? search_text : document.activeElement.previousElementSibling
+            tryFocus(prev)
+            break
+        default:
+            if (e.key.length === 1 || e.key === "Backspace") {
+                search_text.focus()
+            }
+            break
+    }
+
+    function tryFocus(elem: Element) {
+        if (elem === null) return false
+        if (!(elem instanceof HTMLElement)) throw new TypeError()
+        elem.focus()
+    }
+})
 
 search_box.addEventListener("submit", (e: SubmitEvent) => {
     const query = e.submitter.attributes.getNamedItem("value").value
